7279f2dd96bda5fd8925530cd2cdb5be798884c2
veghp <peter.vegh@ncl.ac.uk>
2019-08-28 12:58:15 +0100

    read10xsummary function

---
+++ pyvdj/read10xsummary.py
@@ -0,0 +1,18 @@
+def read10xsummary(paths, samples = None):
+    # This function reads a list of 10x Cell Ranger metrics_summary.csv
+    # filepaths into a pandas dataframe.
+    # samples is a dictionary of path:sample
+    import pandas as pd
+
+    cat_df = pd.DataFrame()
+    for f in paths:
+        df = pd.read_csv(f, thousands=r',')
+        
+        if samples == None:
+            df.insert(0, 'Sample', f)
+        else:
+            df.insert(0, 'Sample', samples[f])
+
+        cat_df = pd.concat([cat_df, df], ignore_index=True)
+
+    return cat_df
