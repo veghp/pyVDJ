06f7175b1265722c5689a5ece4cc9ba154cb768f
veghp <peter.vegh@ncl.ac.uk>
2019-08-29 15:14:30 +0100

    Added add_clonotype() function

--- pyvdj/vdj_add_obs.py
+++ pyvdj/vdj_add_obs.py
@@ -1,24 +1,30 @@
 def add_has_vdjdata(adata):
     obs_col = adata.uns['pyvdj']['obs_col']
     df = adata.uns['pyvdj']['df']
+
     adata.obs['vdj_has_vdjdata'] = adata.obs[obs_col].isin(df['barcode_meta'])
       # 'barcode_meta' is in the df and corresponds to 'obs_col' in anndata.
     return adata
 
-#def vdj_add_obs(adata, obs = ['has_vdjdata'], pyvdj = None):
-def vdj_add_obs(adata, obs = ['has_vdjdata']):
-    # pyvdj is the dictionary returned by vdj_load(), uses adata.uns['pyvdj']
-    # if unspecified.
-#    if pyvdj == None:
-#        pyvdj = adata.uns['pyvdj']
 
-#    obs_col = pyvdj['obs_col']
-#    df = pyvdj['df']
-    
+def add_clonotype(adata):
+    obs_col = adata.uns['pyvdj']['obs_col']
+    df = adata.uns['pyvdj']['df']
+
+    adata.obs['vdj_clonotype'] = adata.obs[obs_col]
+    # Remove missing ones:
+    adata.obs['vdj_clonotype'][-adata.obs['vdj_has_vdjdata']] = None
+
+    tcr_dict = dict(zip(df['barcode_meta'], df['clonotype_meta']))
+    adata.obs['vdj_clonotype'].replace(to_replace=tcr_dict, inplace=True)
+
+    return adata
+
+
+def vdj_add_obs(adata, obs = ['has_vdjdata']):
     adder_functions = {
-        "has_vdjdata": add_has_vdjdata
-        }
-#        "x": 'add_'}
+        "has_vdjdata": add_has_vdjdata,
+        "clonotype": add_clonotype}
 
     for e in obs:
         func = adder_functions[e]
