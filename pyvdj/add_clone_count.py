55f808feb733f7a925ae840a1068da2f00917b49
veghp <peter.vegh@ncl.ac.uk>
2019-08-30 12:34:25 +0100

    Added add_clone_count() function

---
+++ pyvdj/add_clone_count.py
@@ -0,0 +1,14 @@
+def add_clone_count(adata):
+    # Number of clones in clonotype
+    ## add check of adata.obs['vdj_clonotype']
+    clone_count = adata.obs['vdj_clonotype'].value_counts()    
+    clone_count_dict = dict(zip(clone_count.index, clone_count))
+
+    adata.obs['vdj_clone_count'] = adata.obs['vdj_clonotype']
+ 
+    adata.obs['vdj_clone_count'].replace(to_replace=clone_count_dict, inplace=True)
+
+    adata.obs['vdj_clone_count'].loc[adata.obs['vdj_is_clone'] == False] = 1 # not clone (unique)
+    adata.obs['vdj_clone_count'].loc[adata.obs['vdj_has_vdjdata'] == False] = 0 # no data
+
+    return adata
