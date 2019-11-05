d9d5b220700fcfb302df074b856394f9d0371eb0
veghp <peter.vegh@ncl.ac.uk>
2019-11-05 16:25:38 +0000

    Simplified add_gt_n()

--- pyvdj/add_gt_n.py
+++ pyvdj/add_gt_n.py
@@ -18,19 +18,25 @@
 
 def add_gt_n(adata, n):
     # Clonotypes with not less than n clones
-    ## add check of adata.obs['vdj_clonotype']
-    clon_bool = adata.obs['vdj_clonotype'][adata.obs['vdj_is_clone']].value_counts()
+    if (not isinstance(n, int) or n < 0):
+        raise ValueError('n must be a non-negative integer')
 
-    clon_bool[clon_bool < n] = 'Small'
-    clon_bool[clon_bool != 'Small'] = clon_bool[clon_bool != 'Small'].index.tolist()
+    # These two adata.obs columns will be used:
+    if 'vdj_clonotype' not in adata.obs.columns:
+        raise Exception('Please add \'clonotype\' with add_obs()')
+    if 'vdj_clone_count' not in adata.obs.columns:
+        raise Exception('Please add \'clone_count\' with add_obs()')
+
+    clon_bool = adata.obs['vdj_clonotype'].value_counts()
+    txt_less = 'Less_than_n'
+    clon_bool[clon_bool < n] = txt_less
+    clon_bool[clon_bool != txt_less] = clon_bool[clon_bool != txt_less].index.tolist()
     clon_dict = dict(zip(clon_bool.index, clon_bool))
 
-    newcol = 'vdj_clonotype_gt_' + str(n)
+    newcol = 'vdj_clonotype_n_' + str(n)
     adata.obs[newcol] = adata.obs['vdj_clonotype']
-    adata.obs[newcol] = adata.obs[newcol].cat.add_categories(['No_data', 'Small'])
-
-    adata.obs.loc[(-adata.obs['vdj_is_clone']), newcol] = 'Small' # keep order of this
-    adata.obs.loc[(-adata.obs['vdj_has_vdjdata']), newcol] = 'No_data' # and this
+    adata.obs[newcol] = adata.obs[newcol].cat.add_categories(['No_data', txt_less])
+    adata.obs.loc[(-adata.obs['vdj_has_vdjdata']), newcol] = 'No_data'
     adata.obs[newcol].replace(to_replace=clon_dict, inplace=True)
 
     return adata
